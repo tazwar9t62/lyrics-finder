933b6e22f48114f4cb24c29e05f1f9379d1506d7
Tazwar <tazwar9t6@gmail.com>
2020-08-16 23:19:19 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let api = "https://api.lyrics.ovh";
+document.querySelector(".search-btn").addEventListener("click", function () {
+
+    let searchInput = document.getElementById("search-input").value.trim();
+    console.log(searchInput);
+});
+
