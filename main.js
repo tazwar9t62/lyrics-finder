1050d201df1b4ea6d881ef35610c1cbdf11f7b3b
Tazwar <tazwar9t6@gmail.com>
2020-08-16 23:28:37 +0600

    api connected

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 let api = "https://api.lyrics.ovh";
+
+//adding event listener
 document.querySelector(".search-btn").addEventListener("click", function () {
 
     let searchInput = document.getElementById("search-input").value.trim();
-    console.log(searchInput);
+    searchResult(searchInput);
 });
 
+
+
+
+
+//Functions
+//Function to search songs by user's given name 
+function searchResult(name) {
+    fetch(`${api}/suggest/${name}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
