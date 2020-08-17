03061f1ad51d4912ed21560a62ab60450cfba7fc
Tazwar <tazwar9t6@gmail.com>
2020-08-17 09:44:25 +0600

    song suggestion complete!

--- main.js
+++ main.js
@@ -22,27 +22,22 @@ function searchResult(name) {
 }
 
 function showResult(data) {
-    let test = document.getElementById("test");
-    // test.innerText = `${data.data[0].title} by ${data.data[0].artist.name} `
-    test.innerHTML =
-        `<div class="single-result row align-items-center my-3 p-3">
+    let song = data.data;
+    let output = "";
+    for (let index = 0; index < 10; index++) {
+        output +=
+            `<div class="single-result row align-items-center my-3 p-3">
         <div class="col-md-9">
-            <h3 class="track-name">${data.data[0].title}</h3>
-            <p class="author lead"><span>${data.data[0].artist.name}</span></p>
+            <h3 class="track-name">${song[index].title}</h3>
+            <p class="author lead"><span>${song[index].artist.name}</span></p>
         </div>
         <div class="col-md-3 text-md-right text-center">
             <button class="btn btn-success">Lyrics</button>
         </div>
-    </div>`;
-    let test2 = document.getElementById("test2");
-    // test.innerText = `${data.data[0].title} by ${data.data[0].artist.name} `
-    test2.innerHTML = `<div class="single-result row align-items-center my-3 p-3">
-    <div class="col-md-9">
-        <h3 class="track-name">${data.data[1].title}</h3>
-        <p class="author lead"><span>${data.data[1].artist.name}</span></p>
-    </div>
-    <div class="col-md-3 text-md-right text-center">
-        <button class="btn btn-success">Lyrics</button>
-    </div>
-</div>`;
+    </div>`
+    }
+    // console.log(output);
+    let result = document.querySelector(".search-result");
+    result.innerHTML = `
+    ${output}`;
 }
