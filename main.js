2024ba68b9d9bac20860e5f08b55ec057b450772
Tazwar <tazwar9t6@gmail.com>
2020-08-17 08:23:56 +0600

    got song name and artist name

--- main.js
+++ main.js
@@ -16,5 +16,12 @@ document.querySelector(".search-btn").addEventListener("click", function () {
 function searchResult(name) {
     fetch(`${api}/suggest/${name}`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data =>
+            // console.log(data));
+            showResult(data))
+}
+
+function showResult(data) {
+    let test = document.getElementById("test");
+    test.innerText = `${data.data[0].title} by ${data.data[0].artist.name} `
 }
