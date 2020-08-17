9792a788ba94f92252d4ab7538fbe862effecd07
Tazwar <tazwar9t6@gmail.com>
2020-08-17 16:35:06 +0600

    Finally done!

--- main.js
+++ main.js
@@ -46,19 +46,22 @@ function showResult(data) {
     let output = "";
     for (let index = 0; index < 10; index++) {
         output +=
-            `<div class="single-result row align-items-center my-3 p-3 justify-content-between">
-        <div class="col-md-3">
-          
-        </div>
-       <div class="col-md-6">
-            <h3 class="track-name"> <span> ${song[index].title} </span> - <span id="artist">${song[index].artist.name}</span></h3>
-        </div> 
-        <div class="col-md-3 text-md-right text-center">
-            <button class="btn btn-outline-success" id="lyrics-btn" track-name="${song[index].title}" artist-name="${song[index].artist.name}">Lyrics</button>
-        </div>
+            `<div class="single-result align-items-center my-3 p-3 ">
+       <div class="row d-flex justify-content-between">
+       <div class="col-md-3">
+       <img id="img-artist" src="${song[index].artist.picture_small}" alt="">
+       </div>
+      <div class="col-md-6">
+           <h3 class="track-name"> <span> ${song[index].title} </span> - <span id="artist">${song[index].artist.name}</span></h3>
+       </div> 
+       <div class="col-md-3 text-md-right text-center">
+           <button class="btn btn-outline-success" id="lyrics-btn" track-name="${song[index].title}" artist-name="${song[index].artist.name}">Lyrics</button>
+       </div>
+       </div>
     </div>`
         let result = document.querySelector(".search-result");
         result.innerHTML = `${output}`;
+        document.querySelector(".single-lyrics").style.display = "none";
     }
 }
 //Function for getting the lyrics
@@ -73,13 +76,8 @@ function getLyrics(artist_name, track_name) {
             } else {
                 document.querySelector(".lyric").innerText = lyrics;
                 document.querySelector(".titleName").innerText = `${track_name} (${artist_name})`;
-                // pageScroll();
             }
             document.querySelector(".search-result").style.display = "none";
+            document.querySelector(".single-lyrics").style.display = "block";
         })
 }
-// function pageScroll() {
-//     window.scrollBy(0, 1);
-//     scrolldelay = setTimeout(pageScroll, 100);
-// }
-// <img src="${song[index].picture_small}" />
