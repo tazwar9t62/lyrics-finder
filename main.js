25573ce1b6751843ca10442b7655e9dd547a2ceb
Tazwar <tazwar9t6@gmail.com>
2020-08-17 09:17:03 +0600

    before adding full list

--- main.js
+++ main.js
@@ -23,5 +23,26 @@ function searchResult(name) {
 
 function showResult(data) {
     let test = document.getElementById("test");
-    test.innerText = `${data.data[0].title} by ${data.data[0].artist.name} `
+    // test.innerText = `${data.data[0].title} by ${data.data[0].artist.name} `
+    test.innerHTML =
+        `<div class="single-result row align-items-center my-3 p-3">
+        <div class="col-md-9">
+            <h3 class="track-name">${data.data[0].title}</h3>
+            <p class="author lead"><span>${data.data[0].artist.name}</span></p>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button class="btn btn-success">Lyrics</button>
+        </div>
+    </div>`;
+    let test2 = document.getElementById("test2");
+    // test.innerText = `${data.data[0].title} by ${data.data[0].artist.name} `
+    test2.innerHTML = `<div class="single-result row align-items-center my-3 p-3">
+    <div class="col-md-9">
+        <h3 class="track-name">${data.data[1].title}</h3>
+        <p class="author lead"><span>${data.data[1].artist.name}</span></p>
+    </div>
+    <div class="col-md-3 text-md-right text-center">
+        <button class="btn btn-success">Lyrics</button>
+    </div>
+</div>`;
 }
